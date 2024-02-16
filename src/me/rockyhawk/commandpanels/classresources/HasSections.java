1830c900dbc92d31ef32b37400c00044649a6ced
TinyTank800 <tinytank800@gmail.com>
2024-02-16 08:27:35 -0800

    Fix for null players inside hasperm. Returns false on null.

--- src/me/rockyhawk/commandpanels/classresources/HasSections.java
+++ src/me/rockyhawk/commandpanels/classresources/HasSections.java
@@ -12,7 +12,6 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashSet;
-import java.util.Objects;
 
 public class HasSections {
     CommandPanels plugin;
