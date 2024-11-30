1c485a2295ead323deaf3317af5ccb1c4e88dd23
SonnenPingu <126945262+SonnenPingu@users.noreply.github.com>
2024-11-30 14:48:59 +0100

    Create HotbarCommand.java
    Function for hotbar switching.

---
+++ src/me/rockyhawk/commandpanels/commands/HotbarCommand.java
@@ -0,0 +1,35 @@
+// File: HotbarCommand.java
+package me.rockyhawk.commandpanels.openwithitem;
+
+import me.rockyhawk.commandpanels.CommandPanels;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class HotbarCommand implements CommandExecutor {
+    private final HotbarItemLoader hotbarItemLoader;
+
+    public HotbarCommand(HotbarItemLoader hotbarItemLoader) {
+        this.hotbarItemLoader = hotbarItemLoader;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Only players can use this command!");
+            return true;
+        }
+
+        Player player = (Player) sender;
+        boolean status = hotbarItemLoader.toggleHotbarItems(player);
+
+        if (status) {
+            player.sendMessage("Hotbar items enabled.");
+        } else {
+            player.sendMessage("Hotbar items disabled.");
+        }
+
+        return true;
+    }
+}
