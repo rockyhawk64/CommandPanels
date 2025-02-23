bf2a46b5b44a4448c0d20d3ae1d96b88924b99dd
TinyTank800 <TinyTank800@gmail.com>
2025-02-23 12:42:43 -0800

    player-canceled-input: event added.

--- src/me/rockyhawk/commandpanels/interactives/input/PlayerInput.java
+++ src/me/rockyhawk/commandpanels/interactives/input/PlayerInput.java
@@ -9,10 +9,12 @@ public class PlayerInput {
     public Panel panel;
     public ClickType click;
     public List<String> commands;
+    public List<String> cancelCommands;
 
-    public PlayerInput(Panel panel1, List<String> commands1, ClickType click1){
+    public PlayerInput(Panel panel1, List<String> commands1, List<String> cancelCommands1, ClickType click1){
         panel = panel1;
         click = click1;
         commands = commands1;
+        cancelCommands = cancelCommands1;
     }
 }
