f29a7d8322d57272dd98591bd6c134f209666ae2
rockyhawk64 <rockyhawk99@gmail.com>
2025-07-08 18:47:39 +1000

    Removed unused code

--- src/me/rockyhawk/commandpanels/commands/opencommands/BaseCommandPanel.java
+++ src/me/rockyhawk/commandpanels/commands/opencommands/BaseCommandPanel.java
@@ -8,8 +8,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class BaseCommandPanel extends Command {
